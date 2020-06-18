47b79ddffb1755eb6eed82f1fb369fb1203ae9bf
Tom Hacohen <tom@stosb.com>
2020-06-18 13:19:00 +0300

    Remove docusaurus guides
    We are leaving doc1 behind for easy reference.

--- sidebars.js
+++ sidebars.js
@@ -10,13 +10,9 @@ module.exports = {
         'guides/basic_authentication',
         'guides/using_collections',
         'guides/using_items',
-        'doc2',
-        'doc3',
       ],
-      'API Reference': [
-        'mdx',
-        'doc1',
-     ],
+      // 'API Reference': [
+      // ],
     },
   ],
 };
